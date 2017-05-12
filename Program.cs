258e38a25b24fcc4a45c16d94b6e2284a82c7224
suhasdhongade <suhasdhongade@gmail.com>
2017-05-12 10:06:24 +0530

    mmmmmmmmmmm

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace GalaxyBizz
             UserInput.Add("prok is V");
             UserInput.Add("pish is X");
             UserInput.Add("tegj is L");
+
             UserInput.Add("glob glob Silver is 34 Credits");
             UserInput.Add("glob prok Gold is 57800 Credits");
             UserInput.Add("pish pish Iron is 3910 Credits");
