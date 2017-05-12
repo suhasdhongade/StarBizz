e8fc8147d65c056390b2521ad26606bc596984f4
suhasdhongade <suhasdhongade@gmail.com>
2017-05-12 09:56:58 +0530

    asdasddfgdfgdfsdfg

---
+++ Controller/GalaxyNumberSytemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GalaxyBizz.Controller
+{
+    class GalaxyNumberSytemController
+    {
+        GalaxyModel model;
+        ProcessSymbol symbolObj;
+        ProcessMetal metalObj;
+        ProcessQuestion questionObj;
+
+
+        public GalaxyNumberSytemController()
+        {
+            model = new GalaxyModel();
+            metalObj = new ProcessMetal();
+            symbolObj = new ProcessSymbol();
+            questionObj = new ProcessQuestion();
+        }
+        public dynamic StartProcess(string UserEnteredLine)
+        {
+            List<string> inputExtract = Regex.Split(UserEnteredLine, " is ").ToList();
+
+            if (inputExtract[1].ToUpper().EndsWith("CREDITS"))    ///Metal
+            {
+                return metalObj.Process(model, UserEnteredLine, inputExtract);
+            }
+            else if (inputExtract[1].ToUpper().EndsWith("?"))  ///Questions
+            {
+                return questionObj.Process(model, UserEnteredLine, inputExtract);
+            }
+            else if (inputExtract[1].Length == 1) //Reference Data
+            {
+                return symbolObj.Process(model, UserEnteredLine, inputExtract);
+            }
+            else
+            {
+                return "I have no idea what you are talking about";
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using GalaxyBizz.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyBizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<string> UserInput = new List<string>();
+
+
+            UserInput.Add("glob is I");
+            UserInput.Add("prok is V");
+            UserInput.Add("pish is X");
+            UserInput.Add("tegj is L");
+            UserInput.Add("glob glob Silver is 34 Credits");
+            UserInput.Add("glob prok Gold is 57800 Credits");
+            UserInput.Add("pish pish Iron is 3910 Credits");
+            UserInput.Add("how much is pish tegj glob glob?");
+
+            GalaxyNumberSytemController controller = new GalaxyNumberSytemController();
+            foreach (string line in UserInput)
+            {
+                dynamic output = controller.StartProcess(line);
+            } 
+
+        }
+    }
+}
